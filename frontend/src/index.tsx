02d4f648d85eede6bff69ca4b4809c6bde7624eb
Chaehy <chlee7670@cau.ac.kr>
2022-07-31 16:56:19 +0900

    chore: add font , logo tsx files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./fonts/fonts.css";
 
 import rootReducer from "../src/reducers";
 import { createStore, applyMiddleware } from "redux";
